64480575dde7169d58360b57e8217ad969192d6a
mariapaz2005 <mariapazgarciazapata9@gmail.com>
2024-12-05 16:06:12 -0500

    Revert "se suben cambios con el servicio de siniestros funcionando"

--- src/main/java/com/sura/siniestros/Repositorios/IRepositorioSiniestro.java
+++
@@ -1,7 +0,0 @@
-package com.sura.siniestros.Repositorios;
-
-import com.sura.siniestros.Modelos.Siniestro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRepositorioSiniestro extends JpaRepository<Siniestro, Long> {
-}
